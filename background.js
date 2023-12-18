29947b12d55028a5032bacfbfde52ab0ac6a127b
Christopher Lycan <96343159+777388@users.noreply.github.com>
2023-12-17 21:26:44 -0800

    Update background.js

--- background.js
+++ background.js
@@ -11,8 +11,20 @@ chrome.contextMenus.create({
 });
 
 chrome.contextMenus.create({
+    id: "projectlightpdf",
+    title: "projectlightpdf",
+    contexts: ["selection"]
+});
+
+chrome.contextMenus.create({
     id: "researchlight",
     title: "researchlight",
+    contexts: ["selection"]
+});
+
+chrome.contextMenus.create({
+    id: "researchlightpdf",
+    title: "researchlightpdf",
     contexts: ["selection"]
 });
 
@@ -584,10 +596,30 @@ chrome.contextMenus.onClicked.addListener(function (info, tab) {
 });
 
 chrome.contextMenus.onClicked.addListener(function (info, tab) {
+    if (info.menuItemId === "projectlightpdf") {
+        const selectedText = info.selectionText;
+        for (let i = 1; i <= 255; i += 10){
+            const archiveUrl = `https://www.google.com/search?q=%28site:***${i} %7C site:***${i+1} %7C site:***${i+2} %7C site:***${i+3} %7C site:***${i+4} %7C site:***${i+5} %7C site:***${i+6} %7C site:***${i+7} %7C site:***${i+8} %7C site:***${i+9}%29 %26 ${selectedText} %26 Project %26 ext:pdf`;
+            chrome.tabs.create({ url: archiveUrl});
+        }
+    }
+});
+
+chrome.contextMenus.onClicked.addListener(function (info, tab) {
     if (info.menuItemId === "researchlight") {
         const selectedText = info.selectionText;
         for (let i = 1; i <= 255; i += 10){
             const archiveUrl = `https://www.google.com/search?q=%28site:***${i} %7C site:***${i+1} %7C site:***${i+2} %7C site:***${i+3} %7C site:***${i+4} %7C site:***${i+5} %7C site:***${i+6} %7C site:***${i+7} %7C site:***${i+8} %7C site:***${i+9}%29 %26 ${selectedText} %26 research`;
+            chrome.tabs.create({ url: archiveUrl});
+        }
+    }
+});
+
+chrome.contextMenus.onClicked.addListener(function (info, tab) {
+    if (info.menuItemId === "researchlightpdf") {
+        const selectedText = info.selectionText;
+        for (let i = 1; i <= 255; i += 10){
+            const archiveUrl = `https://www.google.com/search?q=%28site:***${i} %7C site:***${i+1} %7C site:***${i+2} %7C site:***${i+3} %7C site:***${i+4} %7C site:***${i+5} %7C site:***${i+6} %7C site:***${i+7} %7C site:***${i+8} %7C site:***${i+9}%29 %26 ${selectedText} %26 research %26 ext:pdf`;
             chrome.tabs.create({ url: archiveUrl});
         }
     }
